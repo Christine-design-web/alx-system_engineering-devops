2110a738630165286e74e43a77c341958e68a2d8
Christine-design-web <wakuthiichristine5@gmail.com>
2023-10-11 15:24:10 +0300

    rewrite

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -5,9 +5,6 @@ $file_to_edit = '/var/www/html/wp-settings.php'
 #replace line containing "phpp" with "php"
 
 exec { 'replace_line':
-  command     => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path        => '/usr/bin:/usr/sbin:/bin:/sbin',
-  onlyif      => "test -f /var/www/html/wp-settings.php",  # Check if the file exists
-  logoutput   => true,  # Log command output for debugging
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
-
